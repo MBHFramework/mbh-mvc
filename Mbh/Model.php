467700a214a989cfab14174546c0ee4326c2f7ca
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-09-25 11:52:02 -0300

    Added model

--- Mbh/Model.php
+++ Mbh/Model.php
@@ -3,19 +3,32 @@
 /**
  * MBHFramework
  *
- * @link      https://github.com/MBHFramework/mbh-framework
- * @copyright Copyright (c) 2017 Ulises Jeremias Cornejo Fandos
- * @license   https://github.com/MBHFramework/mbh-framework/blob/master/LICENSE (MIT License)
+ * @link      https://github.com/MBHFramework/mbh-mvc
+ * @copyright Copyright (c) 2017 COD-Project (https://github.com/COD-Project)
+ * @license   https://github.com/MBHFramework/mbh-mvc/blob/PHP5.6-1.x/LICENSE (MIT License)
  */
 
 namespace Mbh;
 
+use Mbh\Connection\StdConnection as Connection;
+use Mbh\Interfaces\ModelInterface;
+
 /**
- * created by Ulises Jeremias Cornejo Fandos
+ * created by Lucas Di Cunzolo
  */
-class Model
+class Model extends ModelInterface
 {
+    protected $db = null;
+    protected $table = [];
+    protected $columnData = [];
+
     function __construct()
     {
+        $db = Connection::start();
+    }
+
+    function __destruct()
+    {
+        $db = $table = $columnData = null;
     }
 }
